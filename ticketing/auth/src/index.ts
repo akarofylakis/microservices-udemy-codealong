40b44258bc7775d8b5ad0f83f40fa8331e7bd3ee
akarofylakis <akarofylakis@gmail.com>
2020-06-23 22:01:46 +0300

    Added auth service, user model, routes & authentication logic

---
+++ ticketing/auth/src/index.ts
@@ -0,0 +1,55 @@
+import express from 'express';
+import 'express-async-errors';
+import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
+
+import { currentUserRouter } from './routes/current-user';
+import { signInRouter } from './routes/signin';
+import { signOutRouter } from './routes/signout';
+import { signUpRouter } from './routes/signup';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(express.json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
+
+app.use(currentUserRouter);
+app.use(signInRouter);
+app.use(signOutRouter);
+app.use(signUpRouter);
+
+app.all('*', async (req, res, next) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT KEY does not exist.');
+  }
+
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log('Connected to MongoDB');
+  } catch (err) {
+    console.log(err);
+  }
+
+  app.listen(3000, () => {
+    console.log('Listening on port 3000!!!!');
+  });
+};
+
+start();
